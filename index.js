aad27b2e6edfeae18b13e161001d46c165f723cd
ganga.tamang <ganga.bomzan@gmail.com>
2022-09-17 15:55:50 -0400

    test new stuff

--- index.js
+++ index.js
@@ -6,10 +6,16 @@ try {
   const nameToGreet = core.getInput('who-to-greet');
   console.log(`Hello ${nameToGreet}!`);
   const time = (new Date()).toTimeString();
+  const name =  "Resuffled name";
   core.setOutput("time", time);
+  core.setOutput("name",name);
+  core.info("This is information");
+  core.debug("This is a debug");
+  core.error("Error..this is error");
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  // console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
+  core.error(error.message);
 }
